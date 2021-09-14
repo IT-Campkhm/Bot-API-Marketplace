65365f4ce44d24e9de48c35a553a4ef9d4c018dd
xzartsust <68950796+xzartsust@users.noreply.github.com>
2021-09-14 22:03:15 +0300

    Fixed bugs

--- Rozetka/RozetkaOrder.py
+++ Rozetka/RozetkaOrder.py
@@ -35,8 +35,8 @@ class Rozetka:
             new = []
 
             logging.info(f'Lastkey: {self.lastkey}, New Order')
-            if response.json()['content']['orders'][0] is not None:
-                logging.info(f'ID: {response.json()["content"]["orders"][0]["id"]}, New Order')
+            #if response.json()['content']['orders'][0] is not None:
+                #logging.info(f'ID: {response.json()["content"]["orders"][0]["id"]}, New Order')
             logging.info(f'New: {new}, New Order')
             
             if int(self.lastkey) != int(response.json()['content']['orders'][0]['id']) and response.json()['content']['orders'][0]['id'] is not None:
